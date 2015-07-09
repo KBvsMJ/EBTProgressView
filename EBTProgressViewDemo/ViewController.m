6e7d91905b941d15c42066e0dad980aba997daf9
iosbsky2014 <632561914@qq.com>
2015-07-09 21:09:29 +0800

    添加可运行的demo

---
+++ EBTProgressViewDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  EBTProgressViewDemo
+//
+//  Created by MJ on 15/7/9.
+//  Copyright (c) 2015年 TJ. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "EBTProgressView.h"
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet EBTProgressView *progressView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+    _progressView.progress = 0.98;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
